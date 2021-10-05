421bc1aa95efeebddf085461633f076f1532b940
Endermanch-Fan <88939475+Endermanch-Fan@users.noreply.github.com>
2021-10-05 15:30:22 +0200

    The C# Examples
    Examples with CSharp

---
+++ msptdl.cs
@@ -0,0 +1,13 @@
+using System,
+
+namespace NameYourNamespace/*Name Your NameSpace With Your Name*/
+{
+  public class NameYourClass
+  {
+  static void NameYourMethod(string NameYourWord, int NameYourNumber, string[1] NameYourStringWith1Word, bool NameYourStringWithTrueOrFalse, string[] NameYourMultiString)
+  {
+  System.Windows.Forms.MessageBox.Show("Details", "Title", MessageBoxButtons.OK, MessageBoxIcon.Asterisk, MessageBoxDeffaultButton.Button1, MessageBoxOptions.Default);
+  {}[]{}[(3_____________________Hello_How_are_you?]{43}[131]
+  }
+  }
+}
